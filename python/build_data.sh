a92ac1ebc0e67ed27e8de825300c36d9f3461eb8
Sidharth Kapur <sidharth.kapur@affirm.com>
2020-11-25 00:28:23 -0600

    stuff

--- python/build_data.sh
+++ python/build_data.sh
@@ -1,7 +1,8 @@
 yum install -y python3
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 cd python/housing-data
-PATH=PATH:$HOME/.poetry/bin
+export PATH=$PATH:$HOME/.poetry/bin
+echo $PATH
 which python
 poetry install
 poetry run python -m building_data.build_data
