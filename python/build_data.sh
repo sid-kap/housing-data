5d061ef4930a39d9480953de8c7a762c2e397d5e
Sidharth Kapur <sidharth.kapur@affirm.com>
2020-11-25 00:20:39 -0600

    Add build_data.sh script

---
+++ python/build_data.sh
@@ -0,0 +1,5 @@
+yum install -y python3
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+cd python/housing-data
+poetry install
+poetry run python -m building_data.build_data
