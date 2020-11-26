39898b919a8a58ab6fe756607559c581e2ec60a2
Sidharth Kapur <sidharth.kapur@affirm.com>
2020-11-26 02:00:53 -0600

    Add more footers/links so that I can publish.

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Select from "react-select";
 import { useState } from "react";
 import { useStateData } from "../lib/data_loader.js";
 import { VegaLite } from "react-vega";
+import { GitHubFooter } from "../lib/common_elements.js";
 
 const spec = {
   width: 800,
@@ -86,72 +87,38 @@ export default function Home() {
   const data = { table: filterData(response.data, selectedType.value) };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Housing Data</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <div className={styles.container} className="my-0 text-center">
+        <Head>
+          <title>Housing Data</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="mt-1 mb-8 text-4xl col-span-1 text-center">
+          Combined Plots
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+        <Select
+          styles={customStyles}
+          defaultValue={selectedType}
+          onChange={setSelectedOption}
+          options={options}
+        />
+
+        <VegaLite spec={spec} data={data} />
+
+        <p className="m-4 rounded-lg">
+          (The more interesting stuff is in the is&nbsp;
+          <a
+            href="/states/Alabama"
+            className="text-blue-500 hover:text-blue-300"
+          >
+            state-level charts
+          </a>
+          .)
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <Select
-        styles={customStyles}
-        defaultValue={selectedType}
-        onChange={setSelectedOption}
-        options={options}
-      />
-
-      <VegaLite spec={spec} data={data} />
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
+      <GitHubFooter />
     </div>
   );
 }
