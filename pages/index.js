6f8da049ab2ce5b77b4c1b4deabb9bcd893e5d41
Sidharth Kapur <sidharth.kapur@affirm.com>
2020-11-26 22:08:56 -0600

    Fix lint?

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,98 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import LineChart from "./LineChart.js";
-import useFetch from "use-http";
-import Select from "react-select";
-import { useState } from "react";
-import { useStateData } from "../lib/data_loader.js";
-import { VegaLite } from "react-vega";
-import { GitHubFooter } from "../lib/common_elements.js";
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Select from 'react-select'
+import { useState } from 'react'
+import { useStateData } from '../lib/data_loader.js'
+import { VegaLite } from 'react-vega'
+import { GitHubFooter } from '../lib/common_elements.js'
 
 const spec = {
   width: 800,
   height: 600,
   encoding: {
-    x: { field: "year", type: "temporal" },
-    y: { field: "1_unit_units", type: "quantitative" },
-    color: { field: "state_name", type: "nominal" },
+    x: { field: 'year', type: 'temporal' },
+    y: { field: '1_unit_units', type: 'quantitative' },
+    color: { field: 'state_name', type: 'nominal' }
   },
-  data: { name: "table" }, // note: vega-lite data attribute is a plain object instead of an array
-  usermeta: { embedOptions: { renderer: "svg" } },
+  data: { name: 'table' }, // note: vega-lite data attribute is a plain object instead of an array
+  usermeta: { embedOptions: { renderer: 'svg' } },
   layer: [
     {
-      mark: "line",
+      mark: 'line',
       encoding: {
         x: {
-          field: "year",
+          field: 'year',
           scale: {
-            domain: ["1980", "2025"],
-          },
+            domain: ['1980', '2025']
+          }
         },
         y: {
-          field: "1_unit_units",
-        },
+          field: '1_unit_units'
+        }
       },
       tooltip: true,
-      point: true,
+      point: true
     },
     {
-      mark: "text",
+      mark: 'text',
       encoding: {
-        x: { aggregate: "max", field: "year" },
-        y: { aggregate: { argmax: "year" }, field: "1_unit_units" },
-        text: { aggregate: { argmax: "year" }, field: "state_name" },
-      },
-    },
+        x: { aggregate: 'max', field: 'year' },
+        y: { aggregate: { argmax: 'year' }, field: '1_unit_units' },
+        text: { aggregate: { argmax: 'year' }, field: 'state_name' }
+      }
+    }
   ],
   config: {
     text: {
-      align: "left",
+      align: 'left',
       dx: 3,
-      dy: 1,
-    },
-  },
-};
-
-const options = [
-  { value: "state", label: "State" },
-  { value: "region", label: "Region" },
-  { value: "country", label: "Country" },
-  { value: "all", label: "All" },
-];
-
-function filterData(data, type) {
-  if (type == "all") {
-    return data;
-  } else {
-    return data.filter((row) => row.type == type);
+      dy: 1
+    }
   }
 }
 
-export default function Home() {
-  const [request, response] = useStateData();
+const options = [
+  { value: 'state', label: 'State' },
+  { value: 'region', label: 'Region' },
+  { value: 'country', label: 'Country' },
+  { value: 'all', label: 'All' }
+]
 
-  console.log(response.data);
+function filterData (data, type) {
+  if (type === 'all') {
+    return data
+  } else {
+    return data.filter((row) => row.type === type)
+  }
+}
+
+export default function Home () {
+  const { response } = useStateData()
+
+  console.log(response.data)
 
   const [selectedType, setSelectedOption] = useState({
-    value: "all",
-    label: "All",
-  });
+    value: 'all',
+    label: 'All'
+  })
   const customStyles = {
     container: (provided) => ({
       ...provided,
-      width: 150,
-    }),
-  };
+      width: 150
+    })
+  }
 
-  const data = { table: filterData(response.data, selectedType.value) };
+  const data = { table: filterData(response.data, selectedType.value) }
 
   return (
     <div>
-      <div className={styles.container} className="my-0 text-center">
+      <div className={{ ...styles.container, 'my-0': true, 'text-center': true }}>
         <Head>
           <title>Housing Data</title>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel='icon' href='/favicon.ico' />
         </Head>
 
-        <h1 className="mt-1 mb-8 text-4xl col-span-1 text-center">
+        <h1 className='mt-1 mb-8 text-4xl col-span-1 text-center'>
           Combined Plots
         </h1>
 
@@ -107,11 +105,11 @@ export default function Home() {
 
         <VegaLite spec={spec} data={data} />
 
-        <p className="m-4 rounded-lg">
+        <p className='m-4 rounded-lg'>
           (The more interesting stuff is in the is&nbsp;
           <a
-            href="/states/Alabama"
-            className="text-blue-500 hover:text-blue-300"
+            href='/states/Alabama'
+            className='text-blue-500 hover:text-blue-300'
           >
             state-level charts
           </a>
@@ -120,5 +118,5 @@ export default function Home() {
       </div>
       <GitHubFooter />
     </div>
-  );
+  )
 }
--- pages/states/[state].js
+++ pages/states/[state].js
@@ -1,130 +1,134 @@
-import { useRouter } from "next/router";
-import styles from "../../styles/Home.module.css";
-import Head from "next/head";
-import { useStateData } from "../../lib/data_loader.js";
-import LineChart from "../LineChart.js";
-import Select from "react-select";
-import { useState, useEffect } from "react";
-import { VegaLite } from "react-vega";
-import { GitHubFooter } from "../../lib/common_elements.js";
+import { useRouter } from 'next/router'
+import styles from '../../styles/Home.module.css'
+import Head from 'next/head'
+import { useStateData } from '../../lib/data_loader.js'
+import Select from 'react-select'
+import { useState, useEffect } from 'react'
+import { VegaLite } from 'react-vega'
+import { GitHubFooter } from '../../lib/common_elements.js'
 
-function* fields_generator(
-  types = ["bldgs", "units", "value"],
-  suffixes = ["_reported", ""]
+function * fieldsGenerator (
+  types = ['bldgs', 'units', 'value'],
+  suffixes = ['_reported', '']
 ) {
-  for (let num_units of ["1_unit", "2_units", "3_to_4_units", "5_plus_units"]) {
-    for (let type of types) {
-      for (let suffix of suffixes) {
-        yield num_units + "_" + type + suffix;
+  for (const numUnits of [
+    '1_unit',
+    '2_units',
+    '3_to_4_units',
+    '5_plus_units'
+  ]) {
+    for (const type of types) {
+      for (const suffix of suffixes) {
+        yield numUnits + '_' + type + suffix
       }
     }
   }
 }
 
-const fields = Array.from(fields_generator());
+const fields = Array.from(fieldsGenerator())
 
-function spec(units) {
-  const filterFields = Array.from(fields_generator([units], [""]));
+function spec (units) {
+  const filterFields = Array.from(fieldsGenerator([units], ['']))
 
   return {
     width: 800,
     height: 600,
     encoding: {
-      x: { field: "year", type: "temporal" },
-      y: { field: "value", type: "quantitative" },
-      color: { field: "key", type: "nominal" },
+      x: { field: 'year', type: 'temporal' },
+      y: { field: 'value', type: 'quantitative' },
+      color: { field: 'key', type: 'nominal' }
     },
     transform: [
       { fold: fields },
       {
         filter: {
-          field: "key",
-          oneOf: filterFields,
-        },
-      },
+          field: 'key',
+          oneOf: filterFields
+        }
+      }
     ],
-    data: { name: "table" }, // note: vega-lite data attribute is a plain object instead of an array
-    usermeta: { embedOptions: { renderer: "svg" } },
+    data: { name: 'table' }, // note: vega-lite data attribute is a plain object instead of an array
+    usermeta: { embedOptions: { renderer: 'svg' } },
     layer: [
       {
-        mark: "bar",
+        mark: 'bar',
         encoding: {
           x: {
-            field: "year",
+            field: 'year'
           },
           y: {
-            field: "value",
+            field: 'value'
           },
           color: {
-            field: "key",
+            field: 'key',
             scale: {
-              scheme: "tableau10",
-            },
-          },
+              scheme: 'tableau10'
+            }
+          }
         },
-        tooltip: true,
-      },
+        tooltip: true
+      }
     ],
     config: {
       bar: {
-        continuousBandSize: 10,
-      },
-    },
-  };
+        continuousBandSize: 10
+      }
+    }
+  }
 }
 
 const unitsOptions = [
-  { value: "units", label: "Units" },
-  { value: "bldgs", label: "Buildings" },
-  { value: "value", label: "Property value" },
-];
+  { value: 'units', label: 'Units' },
+  { value: 'bldgs', label: 'Buildings' },
+  { value: 'value', label: 'Property value' }
+]
 
 const customStyles = {
   container: (provided) => ({
     ...provided,
-    width: 150,
-  }),
-};
+    width: 150
+  })
+}
 
 const statePickerStyles = {
   container: (provided) => ({
     ...provided,
-    width: 300,
-  }),
-};
+    width: 300
+  })
+}
 
-export default function State() {
-  const router = useRouter();
-  const { state: stateName } = router.query;
+export default function State () {
+  const router = useRouter()
+  const { state: stateName } = router.query
 
-  const [request, response] = useStateData();
+  const { response } = useStateData()
 
   const filteredData = response.data.filter(
-    (row) => row.state_name == stateName
-  );
+    (row) => row.state_name === stateName
+  )
 
-  const data = { table: filteredData };
+  const data = { table: filteredData }
 
   const [selectedUnits, setSelectedUnits] = useState({
-    value: "units",
-    label: "Units",
-  });
+    value: 'units',
+    label: 'Units'
+  })
 
-  const [stateOptions, setStateOptions] = useState([]);
+  const [stateOptions, setStateOptions] = useState([])
 
   useEffect(() => {
     let stateNames = response.data
-      .filter((row) => row.type == "state")
+      .filter((row) => row.type === 'state')
       .map((row) => row.state_name)
-      .filter((row) => row != null);
-    stateNames = Array.from(new Set(stateNames));
+      .filter((row) => row != null)
+    stateNames = Array.from(new Set(stateNames))
     setStateOptions(
       stateNames.map((state) => ({
         value: state,
-        label: state,
+        label: state
       }))
-    );
-  }, [response.status]);
+    )
+  }, [response.status])
 
   return (
     <div>
@@ -132,24 +136,23 @@ export default function State() {
         <title>{stateName}</title>
       </Head>
 
-      <div className="grid grid-cols-3">
+      <div className='grid grid-cols-3'>
         <Select
           styles={statePickerStyles}
           defaultValue={stateName}
           onChange={(newState) =>
-            newState != stateName
-              ? router.push("/states/" + newState.value)
-              : null
-          }
+            newState !== stateName
+              ? router.push('/states/' + newState.value)
+              : null}
           options={stateOptions}
-          className="m-4 col-span-1"
-          placeholder="Change state..."
+          className='m-4 col-span-1'
+          placeholder='Change state...'
           size={0.1}
         />
 
-        <h1 className="mt-4 text-4xl col-span-1 text-center">{stateName}</h1>
+        <h1 className='mt-4 text-4xl col-span-1 text-center'>{stateName}</h1>
 
-        <div className="col-span-1" />
+        <div className='col-span-1' />
       </div>
 
       <div className={styles.container}>
@@ -158,48 +161,48 @@ export default function State() {
           defaultValue={selectedUnits}
           onChange={setSelectedUnits}
           options={unitsOptions}
-          className="m-4"
+          className='m-4'
         />
 
         <VegaLite spec={spec(selectedUnits.value)} data={data} />
 
-        <table className="m-4 table-auto border border-black rounded-lg">
+        <table className='m-4 table-auto border border-black rounded-lg'>
           <thead>
-            <tr className="bg-green-500 text-gray-800">
-              <th className="p-1 border border-black">Year</th>
-              <th className="p-1 border border-black">Single-fam units</th>
-              <th className="p-1 border border-black">2-unit units</th>
-              <th className="p-1 border border-black">3-4 unit units</th>
-              <th className="p-1 border border-black">5+ unit units</th>
+            <tr className='bg-green-500 text-gray-800'>
+              <th className='p-1 border border-black'>Year</th>
+              <th className='p-1 border border-black'>Single-fam units</th>
+              <th className='p-1 border border-black'>2-unit units</th>
+              <th className='p-1 border border-black'>3-4 unit units</th>
+              <th className='p-1 border border-black'>5+ unit units</th>
             </tr>
           </thead>
           <tbody>
             {filteredData.map((value, index) => (
               // toLocaleString adds thousands separator
               <tr key={index}>
-                <td className="p-1 border border-black font-bold">
+                <td className='p-1 border border-black font-bold'>
                   {value.year}
                 </td>
-                <td className="p-1 border border-black">
-                  {value["1_unit_units"].toLocaleString("en")}
+                <td className='p-1 border border-black'>
+                  {value['1_unit_units'].toLocaleString('en')}
                 </td>
-                <td className="p-1 border border-black">
-                  {value["2_units_units"].toLocaleString("en")}
+                <td className='p-1 border border-black'>
+                  {value['2_units_units'].toLocaleString('en')}
                 </td>
-                <td className="p-1 border border-black">
-                  {value["3_to_4_units_units"].toLocaleString("en")}
+                <td className='p-1 border border-black'>
+                  {value['3_to_4_units_units'].toLocaleString('en')}
                 </td>
-                <td className="p-1 border border-black">
-                  {value["5_plus_units_units"].toLocaleString("en")}
+                <td className='p-1 border border-black'>
+                  {value['5_plus_units_units'].toLocaleString('en')}
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
 
-        <p className="m-4 rounded-lg">
+        <p className='m-4 rounded-lg'>
           (See also the&nbsp;
-          <a href="/" className="text-blue-500 hover:text-blue-300">
+          <a href='/' className='text-blue-500 hover:text-blue-300'>
             combined charts
           </a>
           .)
@@ -207,5 +210,5 @@ export default function State() {
       </div>
       <GitHubFooter />
     </div>
-  );
+  )
 }
