5f274a648d4a4b4f07f4493f57247e74080241d2
Sidharth Kapur <sidharth.kapur@affirm.com>
2020-11-29 14:44:01 -0600

    Fix build

--- python/housing-data/housing_data/build_data.py
+++ python/housing-data/housing_data/build_data.py
@@ -6,14 +6,14 @@ from tqdm import tqdm
 
 
 def main():
-    # dfs = []
-    # for year in range(1980, 2020):
-    #     data = bps.load_data(
-    #         scale="state", time_scale="annual", year=year, month=None, region=None
-    #     ).assign(year=str(year))
-    #     dfs.append(data)
+    dfs = []
+    for year in range(1980, 2020):
+        data = bps.load_data(
+            scale="state", time_scale="annual", year=year, month=None, region=None
+        ).assign(year=str(year))
+        dfs.append(data)
 
-    # pd.concat(dfs).to_json("../../public/state_annual.json", orient="records")
+    pd.concat(dfs).to_json("../../public/state_annual.json", orient="records")
 
     dfs = []
     for year in range(1980, 2020):
@@ -25,7 +25,7 @@ def main():
 
     places_df = pd.concat(dfs)
 
-    places_df.to_json("../../public/place_annual.json", orient="records")
+    # places_df.to_json("../../public/place_annual.json", orient="records")
     (
         places_df[["place_name", "state_code"]]
         .drop_duplicates()
@@ -41,7 +41,6 @@ def main():
         group.reset_index(drop=True).to_json(
             Path(path, f"{place_name}.json"), orient="records"
         )
-    # pd.concat(dfs, ignore_index=True).to_feather("../../public/place_annual.feather")
 
 
 if __name__ == "__main__":
