2b74029339c0e8e18e9c9deb18958006211b6173
Sidharth Kapur <sidharth.kapur@affirm.com>
2020-12-24 23:25:21 -0600

    Move python package to different folder

--- build_data.sh
+++ build_data.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 yum install -y python3
 pip3 install --user poetry
-cd python/housing-data || exit
+cd python || exit
 export PATH=$PATH:$HOME/.local/bin
 poetry install --no-dev
 poetry run build_data
