10ec95c75b8808780fcc99db688a84edf7662797
Sidharth Kapur <sidharthkapur1@gmail.com>
2021-05-07 23:32:31 -0700

    Undo build_data.sh change

--- build_data.sh
+++ build_data.sh
@@ -1,10 +1,7 @@
 #! /usr/bin/env bash
-yum install -y wget
-wget https://www2.census.gov/econ/bps/State/st1980a.txt
-cat st1980a.txt
-# yum install -y python3
-# pip3 install --user poetry
-# cd python || exit
-# export PATH=$PATH:$HOME/.local/bin
-# poetry install --no-dev
-# poetry run build_data
+yum install -y python3
+pip3 install --user poetry
+cd python || exit
+export PATH=$PATH:$HOME/.local/bin
+poetry install --no-dev
+poetry run build_data
