06e55bc51ff7c06d261fad9bbf2791d2d8c80c3b
Sid Kapur <sid-kap@users.noreply.github.com>
2021-05-09 01:08:08 -0700

    Add optimization to tailwind (#32)
    Purge unused tailwind styles. This should reduce the outputted CSS from 300 kB to 3 kB.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+      './lib/**/*.js',
+      './pages/**/*.js'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
