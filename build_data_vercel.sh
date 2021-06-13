6e7af8ffc968b94a918d919ded01838d15df8c3a
Sidharth Kapur <sidharthkapur1@gmail.com>
2021-06-13 15:53:44 -0700

    stuff

--- build_data_vercel.sh
+++ build_data_vercel.sh
@@ -5,4 +5,4 @@ cd python || exit
 export PATH=$PATH:$HOME/.local/bin
 poetry install --no-dev
 poetry run build_data --use-data-repo
-git clone https://github.com/YIMBYdata/housing-elements && mv housing-elements/map_results public/rhna_data
+GIT_LFS_SKIP_SMUDGE=1 git clone https://github.com/YIMBYdata/housing-elements && mv housing-elements/map_results public/rhna_data
