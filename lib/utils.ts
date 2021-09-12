5349b7a887e4baf77d63a2579327e1d96365511d
Sidharth Kapur <sidharthkapur1@gmail.com>
2021-09-12 13:56:59 -0700

    fix github lint?

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,18 +1,8 @@
-function makeIndexLookup(placesList, nameField) {
-  const lookupTable = {}
-
-  for (let i = 0; i < placesList.length; i++) {
-    lookupTable[placesList[i][nameField]] = i
-  }
-
-  return lookupTable
-}
-
 export class PathMapping {
-    placesList: object[]
-    mapping: Map<string, number>
+  placesList: object[]
+  mapping: Map<string, number>
   // Class that makes it easy to get from page path to the relevant row in the place list.
-    constructor(placesList: object[], pathFn: (object) => string = (e) => e.path) {
+  constructor(placesList: object[], pathFn: (object) => string = (e) => e.path) {
     this.placesList = placesList
     this.mapping = new Map()
 
