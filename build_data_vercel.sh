38cb408b1514e3f35f6ae62814c63ff8a7134f89
Sidharth Kapur <sidharthkapur1@gmail.com>
2021-12-23 18:43:18 -0600

    fix

--- build_data_vercel.sh
+++ build_data_vercel.sh
@@ -7,4 +7,5 @@ cd python || exit
 export PATH=$PATH:$HOME/.local/bin
 poetry install --no-dev
 git clone https://github.com/sid-kap/housing-data-data ../housing-data-data
+ls ../housing-data-data
 poetry run build_data --data-repo-path ../housing-data-data
