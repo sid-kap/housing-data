ad96d4d90cfe42cefbe173c5019953db845f47b0
Sidharth Kapur <sidharthkapur1@gmail.com>
2022-07-29 13:20:40 -0400

    Add June 2022 data

--- lib/projections.tsx
+++ lib/projections.tsx
@@ -16,7 +16,7 @@ const months = {
   12: "December",
 }
 
-const latestMonth: number = 5
+const latestMonth: number = 6
 const glueWord = latestMonth == 2 ? "and" : "through"
 const observedMonths =
   latestMonth == 1
