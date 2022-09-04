092d1b26078f89a184af0bcbfcb124cb2ce5b781
Sid Kapur <sid@openlabsusa.org>
2022-09-04 19:51:33 -0400

    Update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === "production",
-    content: [
-      "./lib/**/*.js",
-      "./lib/**/*.jsx",
-      "./lib/**/*.ts",
-      "./lib/**/*.tsx",
-      "./pages/**/*.js",
-      "./pages/**/*.jsx",
-      "./pages/**/*.ts",
-      "./pages/**/*.tsx",
-    ],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./lib/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     screens: {
       sm: "640px",
@@ -22,9 +12,6 @@ module.exports = {
       xl: "1280px",
       "2xl": "1536px",
     },
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
