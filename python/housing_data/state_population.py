sid-kap/housing-data

commit 11edc3df3a530a803ad72c2007cf9ba3504984b3

Update latest population data from 2020 to 2022 (#78)
I.e. stop imputing 2021, 2022, 2023 population as the 2020 population

python/housing_data/state_population.py
@@ -1,16 +1,11 @@
-from __future__ import annotations
-
 from io import StringIO
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import List, Optional
 
 import pandas as pd
 import us
-from housing_data.build_data_utils import impute_2020s_population
+from housing_data.build_data_utils import impute_2023_population
 from housing_data.data_loading_helpers import get_path, get_url_text
-
-if TYPE_CHECKING:
-    from typing import List, Optional
 
 DIVISIONS = {
     "New England": [
@@ -207,33 +202,13 @@ def get_state_populations_2000s(data_path: Optional[Path]) -> pd.DataFrame:
     return df.melt(id_vars="state", var_name="year", value_name="population")
 
 
-def get_state_populations_2010_through_2019(
-    data_path: Optional[Path],
-) -> pd.DataFrame:
-    """
-    This function is not used anymore
-    """
-    df = pd.read_excel(
-        get_path(
-            "https://www2.census.gov/programs-surveys/popest/tables/2010-2019/state/totals/nst-est2019-01.xlsx",
-            data_path,
-        ),
-        skiprows=3,
-        skipfooter=5,
+def _melt_df(df: pd.DataFrame, years: List[int]) -> pd.DataFrame:
+    return (
+        df[["NAME"] + [f"POPESTIMATE{year}" for year in years]]
+        .rename(columns={f"POPESTIMATE{year}": str(year) for year in years})
+        .rename(columns={"NAME": "state"})
+        .melt(id_vars=["state"], var_name="year", value_name="population")
     )
-    df = df.rename(columns={"Unnamed: 0": "state"}).dropna(subset=["state"])
-
-    df.columns = df.columns.astype(str)
-
-    # IDK what to do with this
-    # (also not sure what the difference is between the "Census", "Estimates Base", and "2010" columns
-    df = df.drop(columns=["Census", "Estimates Base"])
-
-    df["state"] = df["state"].str.lstrip(".")
-
-    df = df.astype({col: int for col in df.columns if col != "state"})
-
-    return df.melt(id_vars="state", var_name="year", value_name="population")
 
 
 def get_state_populations_2010s(data_path: Optional[Path]) -> pd.DataFrame:
@@ -247,12 +222,19 @@ def get_state_populations_2010s(data_path: Optional[Path]) -> pd.DataFrame:
         )
     )
 
-    return (
-        df[["NAME"] + [f"POPESTIMATE{year}" for year in range(2010, 2021)]]
-        .rename(columns={f"POPESTIMATE{year}": str(year) for year in range(2010, 2021)})
-        .rename(columns={"NAME": "state"})
-        .melt(id_vars=["state"], var_name="year", value_name="population")
+    return _melt_df(df, list(range(2010, 2021)))
+
+
+def get_state_populations_2020s(data_path: Optional[Path]) -> pd.DataFrame:
+    df = pd.read_csv(
+        get_path(
+            "https://www2.census.gov/programs-surveys/popest/datasets/2020-2022/state/totals/NST-EST2022-ALLDATA.csv",
+            data_path,
+        )
     )
+
+    df = _melt_df(df, list(range(2020, 2023)))
+    return impute_2023_population(df)
 
 
 def get_state_population_estimates(data_path: Optional[Path]) -> pd.DataFrame:
@@ -268,7 +250,8 @@ def get_state_population_estimates(data_path: Optional[Path]) -> pd.DataFrame:
     print("Loading 2010s data...")
     df_2010s = get_state_populations_2010s(data_path)
 
-    df_2020s = impute_2020s_population(df_2010s)
+    print("Loading 2020s data...")
+    df_2020s = get_state_populations_2020s(data_path)
 
     states_df = pd.concat([df_1980s, df_1990s, df_2000s, df_2010s, df_2020s])
 

python/housing_data/build_data_utils.py
@@ -201,19 +201,13 @@ def add_current_year_projections(year_to_date_df: pd.DataFrame) -> pd.DataFrame:
     return year_to_date_df
 
 
-def impute_2020s_population(df_2010s: pd.DataFrame) -> pd.DataFrame:
+def impute_2023_population(df_2020s: pd.DataFrame) -> pd.DataFrame:
     """
-    Impute 2021 and 2022 with the 2020 population; that's the best I think we can do
-    until the intercensals start coming out...
-    (I guess we could use the overall US population growth rate, but that's a little sketchy for
-    slow-growing metros...)
+    Impute 2023 with the 2022 population
     """
-    # TODO switch to the 2020-21 intercensal dataset
-    # (https://www2.census.gov/programs-surveys/popest/datasets/2020-2021/cities/totals/)
     return pd.concat(
         [
-            df_2010s[df_2010s["year"] == "2020"].assign(year="2021"),
-            df_2010s[df_2010s["year"] == "2020"].assign(year="2022"),
-            df_2010s[df_2010s["year"] == "2020"].assign(year="2023"),
+            df_2020s,
+            df_2020s[df_2020s["year"] == "2022"].assign(year="2023"),
         ]
     )

python/housing_data/county_population.py
@@ -1,16 +1,34 @@
-from __future__ import annotations
-
 from io import StringIO
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import List, Optional
 
 import pandas as pd
 import us
-from housing_data.build_data_utils import impute_2020s_population
+from housing_data.build_data_utils import impute_2023_population
 from housing_data.data_loading_helpers import get_path, get_url_text
 
-if TYPE_CHECKING:
-    from typing import Optional
+
+def _melt_df(df: pd.DataFrame, years: List[int]) -> pd.DataFrame:
+    rename_cols = {"STATE": "state_code", "COUNTY": "county_code"} | {
+        "POPESTIMATE" + str(year): str(year) for year in years
+    }
+
+    df = df[rename_cols.keys()].rename(columns=rename_cols)
+    return df.melt(
+        id_vars=["county_code", "state_code"], var_name="year", value_name="population"
+    )
+
+
+def get_county_populations_2020s(data_path: Optional[Path]) -> pd.DataFrame:
+    df = pd.read_csv(
+        get_path(
+            "https://www2.census.gov/programs-surveys/popest/datasets/2020-2022/counties/totals/co-est2022-alldata.csv",
+            data_path,
+        ),
+    )
+
+    df = _melt_df(df, list(range(2020, 2023)))
+    return impute_2023_population(df)
 
 
 def get_county_populations_2010s(data_path: Optional[Path]) -> pd.DataFrame:
@@ -22,16 +40,7 @@ def get_county_populations_2010s(data_path: Optional[Path]) -> pd.DataFrame:
         encoding="latin_1",
     )
 
-    rename_cols = {"POPESTIMATE" + str(year): str(year) for year in range(2010, 2021)}
-    rename_cols.update({"STATE": "state_code", "COUNTY": "county_code"})
-
-    df = df[rename_cols.keys()].rename(columns=rename_cols)
-
-    df = df.melt(
-        id_vars=["county_code", "state_code"], var_name="year", value_name="population"
-    )
-
-    return df
+    return _melt_df(df, list(range(2010, 2020)))
 
 
 def get_county_populations_2000s(data_path: Optional[Path]) -> pd.DataFrame:
@@ -109,7 +118,7 @@ def get_county_populations_2000s(data_path: Optional[Path]) -> pd.DataFrame:
 def get_county_fips_crosswalk(data_path: Optional[Path]) -> pd.DataFrame:
     df = pd.read_excel(
         get_path(
-            "https://www2.census.gov/programs-surveys/popest/geographies/2019/all-geocodes-v2019.xlsx",
+            "https://www2.census.gov/programs-surveys/popest/geographies/2021/all-geocodes-v2021.xlsx",
             data_path,
         ),
         skiprows=4,
@@ -232,8 +241,8 @@ def get_county_population_estimates(data_path: Optional[Path]) -> pd.DataFrame:
     df_2000s = get_county_populations_2000s(data_path)
     print("Loading 2010s populations...")
     df_2010s = get_county_populations_2010s(data_path)
-
-    df_2020s = impute_2020s_population(df_2010s)
+    print("Loading 2020s populations...")
+    df_2020s = get_county_populations_2020s(data_path)
 
     df = pd.concat([df_1980s, df_1990s, df_2000s, df_2010s, df_2020s])
 

python/housing_data/place_population.py
@@ -1,16 +1,11 @@
-from __future__ import annotations
-
 from io import StringIO
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import List, Optional
 
 import numpy as np
 import pandas as pd
-from housing_data.build_data_utils import impute_2020s_population
+from housing_data.build_data_utils import impute_2023_population
 from housing_data.data_loading_helpers import get_path, get_url_text
-
-if TYPE_CHECKING:
-    from typing import List, Optional
 
 
 def _get_places_crosswalk_df(data_path: Optional[Path] = None) -> pd.DataFrame:
@@ -261,7 +256,7 @@ def _fix_place_names(place_names: pd.Series) -> pd.Series:
     return place_names
 
 
-def remove_dupe_cities(df: pd.DataFrame) -> pd.DataFrame:
+def remove_duplicate_cities(df: pd.DataFrame) -> pd.DataFrame:
     """
     If there are two places in the same state with different place_fips codes, then fuck it I have no idea which city
     s which (recall that BPS before 2000 doesn't have place FIPS codes---they may not have existed back then).
@@ -322,9 +317,10 @@ def get_place_populations_1990s(data_path: Optional[Path]) -> pd.DataFrame:
         .reset_index()
     )
 
-    rename_dict = {"place": "place_name", "state_fips": "state_code"}
-    rename_dict.update({f"{year}-07-01": f"{year}" for year in range(1990, 2001)})
-    combined_df = combined_df.rename(columns=rename_dict)
+    combined_df = combined_df.rename(
+        columns={"place": "place_name", "state_fips": "state_code"}
+        | {f"{year}-07-01": f"{year}" for year in range(1990, 2001)}
+    )
 
     # First confirm that the only dupes (same place and state) is if they have different fips
     assert (
@@ -337,7 +333,7 @@ def get_place_populations_1990s(data_path: Optional[Path]) -> pd.DataFrame:
         == 0
     )
 
-    combined_df = remove_dupe_cities(combined_df)
+    combined_df = remove_duplicate_cities(combined_df)
 
     combined_df = combined_df.drop(columns=["state_abbr", "place_fips"])
 
@@ -348,28 +344,33 @@ def get_place_populations_1990s(data_path: Optional[Path]) -> pd.DataFrame:
     )
 
 
-def _get_recent_decades_df(
-    url: str, has_consolidated_cities: bool, years: List[int]
+def _melt_df(
+    df: pd.DataFrame,
+    years: List[int],
+    encoding: Optional[str] = None,
+    has_consolidated_cities: bool = True,
 ) -> pd.DataFrame:
-    df = pd.read_csv(url, encoding="latin_1")
+    """
+    For 2000s, 2010s, and 2020s
+    """
+    if f"POPESTIMATE{years[0]}" not in df.columns:
+        # POPESTIMATE2020 isn't present in the 2020s file
+        df = df.rename(columns={f"ESTIMATESBASE{years[0]}": f"POPESTIMATE{years[0]}"})
 
-    state_rows = (df["COUNTY"] == 0) & (df["PLACE"] == 0)
-
-    if has_consolidated_cities:
-        not_consolidated_city_rows = df["CONCIT"] == 0
-    else:
-        not_consolidated_city_rows = pd.Series(True, index=df.index)
-
-    # WOWWWW the format is so nice!!! makes things so easy!!
-    df = df[
+    mask = (
         (
             (df["COUNTY"] == 0)  # indicates total of a city across all counties it's in
             | (df["PLACE"] == 99990)  # indicates a "Balance of county" record
         )
         & (df["COUSUB"] == 0)  # remove townships and shit (county subdivisions)
-        & not_consolidated_city_rows
-        & ~state_rows  # remove states
-    ].copy()
+        & ~((df["COUNTY"] == 0) & (df["PLACE"] == 0))  # remove states
+    )
+
+    if has_consolidated_cities:
+        # Needed for 2010s and 2020s
+        mask &= df["CONCIT"] == 0
+
+    df = df[mask].copy()
 
     df["place_or_county_code"] = (
         df["PLACE"]
@@ -377,16 +378,14 @@ def _get_recent_decades_df(
         .where(df["PLACE"] != 99990, df["COUNTY"].astype(str) + "_county")
     )
 
-    df = df[
-        ["NAME", "STATE", "place_or_county_code"]
-        + [f"POPESTIMATE{year}" for year in years]
-    ]
+    cols = {
+        "NAME": "place_name",
+        "STATE": "state_code",
+        "place_or_county_code": "place_or_county_code",
+    } | {f"POPESTIMATE{year}": f"{year}" for year in years}
+    df = df[cols.keys()].rename(columns=cols)
 
-    rename_dict = {"NAME": "place_name", "STATE": "state_code"}
-    rename_dict.update({f"POPESTIMATE{year}": f"{year}" for year in years})
-    df = df.rename(columns=rename_dict)
-
-    df = remove_dupe_cities(df)
+    df = remove_duplicate_cities(df)
 
     df["place_name"] = df["place_name"].str.replace("^Balance of ", "", regex=True)
 
@@ -398,27 +397,43 @@ def _get_recent_decades_df(
 
 
 def get_place_populations_2000s(data_path: Optional[Path]) -> pd.DataFrame:
-    # This one doesn't include consolidated cities, so no need to remove those rows
-    return _get_recent_decades_df(
+    df = pd.read_csv(
         get_path(
             "https://www2.census.gov/programs-surveys/popest/datasets/2000-2010/intercensal/cities/sub-est00int.csv",
             data_path,
         ),
-        has_consolidated_cities=False,
+        encoding="latin_1",
+    )
+    return _melt_df(
+        df,
         years=list(range(2000, 2011)),
+        # This one doesn't include consolidated cities, so no need to remove those rows
+        has_consolidated_cities=False,
     )
 
 
 def get_place_populations_2010s(data_path: Optional[Path]) -> pd.DataFrame:
-    # This one has consolidated cities that need to be removed
-    return _get_recent_decades_df(
+    df = pd.read_csv(
         get_path(
             "https://www2.census.gov/programs-surveys/popest/datasets/2010-2020/cities/SUB-EST2020_ALL.csv",
             data_path,
         ),
-        has_consolidated_cities=True,
-        years=list(range(2010, 2021)),
+        encoding="latin_1",
     )
+
+    return _melt_df(df, years=list(range(2010, 2021)))
+
+
+def get_place_populations_2020s(data_path: Optional[Path]) -> pd.DataFrame:
+    df = pd.read_csv(
+        get_path(
+            "https://www2.census.gov/programs-surveys/popest/datasets/2010-2020/cities/sub-est2022.csv",
+            data_path,
+        ),
+    )
+    df = _melt_df(df, years=list(range(2020, 2023)))
+    df = impute_2023_population(df)
+    return df
 
 
 def interpolate_1980s_populations(
@@ -486,6 +501,8 @@ def get_place_population_estimates(data_path: Optional[Path] = None) -> pd.DataF
     df_2000s = get_place_populations_2000s(data_path)
     print("Loading 2010s populations...")
     df_2010s = get_place_populations_2010s(data_path)
+    print("Loading 2020s populations...")
+    df_2020s = get_place_populations_2020s(data_path)
 
     # Remove the dupes by only taking [1990, 2000) from the 90s dataset,
     # [2000, 2010) from the 2000s dataset, etc. since these decade ones have both the start and end year.
@@ -498,12 +515,9 @@ def get_place_population_estimates(data_path: Optional[Path] = None) -> pd.DataF
     # This would help with the jumps we see from 1999 to 2000, and from 2009 to 2010 (you can see this in Google too)
     df_1990s = df_1990s[df_1990s["year"] != "2000"]
     df_2000s = df_2000s[df_2000s["year"] != "2010"]
+    df_2010s = df_2010s[df_2010s["year"] != "2020"]
 
     print("Interpolating 1980s populations...")
     interp_df = interpolate_1980s_populations(df_1980, df_1990s)
 
-    df_2020s = impute_2020s_population(df_2010s)
-
-    combined_df = pd.concat([interp_df, df_1990s, df_2000s, df_2010s, df_2020s])
-
-    return combined_df
+    return pd.concat([interp_df, df_1990s, df_2000s, df_2010s, df_2020s])

commit 3e174a7d3ea11e666c56a0b83d96b9c241eb027b

Remove duplicate row for 2020 data (#79)
The bar for 2020 shouldn't be twice as high as 2019 and 2021...

This is a bug introduced in #78. We have two population rows for 2020 (one from the 2010s dataset, one from the 2020s dataset), and when it gets joined to the BPS data we get two identical rows for 2020 for each state. This makes the bar look twice as high

python/housing_data/state_population.py
@@ -212,9 +212,6 @@ def _melt_df(df: pd.DataFrame, years: List[int]) -> pd.DataFrame:
 
 
 def get_state_populations_2010s(data_path: Optional[Path]) -> pd.DataFrame:
-    """
-    This one goes through 2020
-    """
     df = pd.read_csv(
         get_path(
             "https://www2.census.gov/programs-surveys/popest/datasets/2010-2020/state/totals/nst-est2020-alldata.csv",
@@ -222,7 +219,7 @@ def get_state_populations_2010s(data_path: Optional[Path]) -> pd.DataFrame:
         )
     )
 
-    return _melt_df(df, list(range(2010, 2021)))
+    return _melt_df(df, list(range(2010, 2020)))
 
 
 def get_state_populations_2020s(data_path: Optional[Path]) -> pd.DataFrame: