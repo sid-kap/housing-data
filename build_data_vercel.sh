e8abc23c13413baaeb04f5f5f76d548b093d0ae2
Sid Kapur <sid@openlabsusa.org>
2023-05-27 20:11:08 -0400

    see how long building takes

--- build_data_vercel.sh
+++ build_data_vercel.sh
@@ -1,7 +1,14 @@
 #! /usr/bin/env bash
 
-amazon-linux-extras install python3.10
-python3.10
+# amazon-linux-extras install python3.10
+# python3.10
+
+wget https://www.python.org/ftp/python/3.10.11/Python-3.10.11.tgz
+tar xzf Python-3.10.11.tgz
+cd Python-3.10.11
+sudo ./configure --enable-optimizations
+sudo make altinstall
+rm -rf Python-3.10.11.tgz
 
 pip3 install --user poetry
 cd python || exit
