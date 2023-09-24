7d3adc72da190b05a728513d6911d43e4abcdd24
Sid Kapur <sid@openlabsusa.org>
2023-09-23 21:12:19 -0400

    remove python3.9

--- build_data_vercel.sh
+++ build_data_vercel.sh
@@ -1,8 +1,5 @@
 #! /usr/bin/env bash
 
-# Python 3.9, last time I checked
-yum install -y python3
-
 # Fixes this error: https://urllib3.readthedocs.io/en/latest/v2-migration-guide.html#ssl-module-is-compiled-with-openssl-1-0-2-k-fips
 # TODO: remove when Vercel upgrades to Amazon Linux 2023
 yum remove openssl openssl-devel
