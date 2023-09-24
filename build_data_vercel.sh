8709357f2ba34e3eab856442a8489df51d4fd2c9
Sid Kapur <sidharthkapur1@gmail.com>
2023-09-23 21:58:47 -0400

    Fix openssl build error, upgrade to python3.11 (#83)

--- build_data_vercel.sh
+++ build_data_vercel.sh
@@ -1,7 +1,18 @@
 #! /usr/bin/env bash
 
-# Python 3.9, last time I checked
-yum install -y python3
+# Fixes this error: https://urllib3.readthedocs.io/en/latest/v2-migration-guide.html#ssl-module-is-compiled-with-openssl-1-0-2-k-fips
+# TODO: remove when Vercel upgrades to Amazon Linux 2023
+yum remove openssl openssl-devel
+yum update openssl11 openssl11-devel
+yum install -y openssl11 openssl11-devel
+
+curl https://pyenv.run | bash
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+
+pyenv install 3.11.5
+pyenv global 3.11.5
 
 pip3 install --user poetry
 cd python || exit
