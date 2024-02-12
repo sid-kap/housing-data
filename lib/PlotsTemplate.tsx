fbf0bad77f193a49799aebee8996ab7fcb6176d7
Sid Kapur <sid@openlabsusa.org>
2024-02-12 18:06:56 -0500

    fix

--- lib/PlotsTemplate.tsx
+++ lib/PlotsTemplate.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 
 import BarPlot from "lib/BarPlot"
 import { CurrentYearExtrapolationInfo } from "lib/projections"
