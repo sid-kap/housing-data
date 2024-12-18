d6a7234dcb550f5e1efabc3c822f07742be9291f
Sid Kapur <sid@openlabsusa.org>
2024-12-18 15:53:25 -0500

    does this fix it?

--- build_data_vercel.sh
+++ build_data_vercel.sh
@@ -2,7 +2,7 @@
 
 # Fixes this error: https://urllib3.readthedocs.io/en/latest/v2-migration-guide.html#ssl-module-is-compiled-with-openssl-1-0-2-k-fips
 # TODO: remove when Vercel upgrades to Amazon Linux 2023
-yum remove openssl openssl-devel
+# yum remove openssl openssl-devel
 yum update openssl11 openssl11-devel
 yum install -y openssl11 openssl11-devel
 
