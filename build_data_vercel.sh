607b64218b566deb41043cc6e5a33a14758da1bc
Sid Kapur <sid@openlabsusa.org>
2024-12-18 15:55:36 -0500

    We're on Amazon Linux 2023 now, yay

--- build_data_vercel.sh
+++ build_data_vercel.sh
@@ -1,11 +1,5 @@
 #! /usr/bin/env bash
 
-# Fixes this error: https://urllib3.readthedocs.io/en/latest/v2-migration-guide.html#ssl-module-is-compiled-with-openssl-1-0-2-k-fips
-# TODO: remove when Vercel upgrades to Amazon Linux 2023
-# yum remove openssl openssl-devel
-yum update openssl11 openssl11-devel
-yum install -y openssl11 openssl11-devel
-
 curl https://pyenv.run | bash
 
 export PATH="$HOME/.pyenv/bin:$PATH"
