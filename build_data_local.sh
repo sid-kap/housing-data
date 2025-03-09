843e46ff7389295a4a3fac079dd358aff6a52dbb
Sid Kapur <sid@openlabsusa.org>
2025-03-09 13:09:18 -0400

    use uv locally too

--- build_data_local.sh
+++ build_data_local.sh
@@ -3,6 +3,6 @@ pushd python/ || exit
 
 # Assumes that https://github.com/sid-kap/housing-data-data has been cloned at the same level as the
 # housing-data repo.
-poetry run build_data --data-repo-path ../../housing-data-data
+uv run python -m housing_data.build_data --data-repo-path ../../housing-data-data
 
 popd || exit
