7b04cd0274a5719a72e04885d157b74970935874
Sid Kapur <sid@openlabsusa.org>
2025-07-13 02:12:37 -0400

    Add May 2025 data

--- python/housing_data/build_data_utils.py
+++ python/housing_data/build_data_utils.py
@@ -80,8 +80,8 @@ CANADA_CROSSWALK_DIR = Path("data", "canada-crosswalk")
 CANADA_POPULATION_DIR = Path("data", "canada-population")
 
 # Last year and month for which monthly BPS data is available (and is cloned to housing-data-data).
-LATEST_MONTH = (2025, 3)
-LAST_YEAR_ANNUAL_DATA_RELEASED = False
+LATEST_MONTH = (2025, 5)
+LAST_YEAR_ANNUAL_DATA_RELEASED = True
 
 
 def write_to_json_directory(df: pd.DataFrame, path: Path) -> None:
