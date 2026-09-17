2492be3ba509aaea352490be1818a597cc705f12
Sid Kapur <sid@openlabsusa.org>
2026-09-17 18:20:45 -0500

    debug

--- python/housing_data/building_permits_survey.py
+++ python/housing_data/building_permits_survey.py
@@ -200,6 +200,7 @@ def get_data_path(
 def read_bps_formatted_csv(
     csv_contents: str, scale: Scale, year: int, region: Optional[Region] = None
 ) -> pd.DataFrame:
+    print(csv_contents)
     """
     Given the contents of a CSV file from the BPS dataset, parses it as a DataFrame.
     Takes into account several quirks in the way they format their files.
@@ -256,6 +257,7 @@ def load_data(
     if ERROR_STRING in text:
         raise ValueError(f"Path {path} is not valid")
 
+    print(text, scale, year, region)
     df = read_bps_formatted_csv(text, scale, year, region)
 
     if scale == "state":
